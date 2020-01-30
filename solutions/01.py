4eef70046dc65c7519a44e8dec7856a3041c4490
John Harvey <johnharveybc@gmail.com>
2020-01-29 20:43:37 -0800

    Add solution for day 1, part 2

--- solutions/01.py
+++ solutions/01.py
@@ -6,15 +6,31 @@ def main():
 
     with open(input_file, 'r') as fh:
         part_1 = 0
+        part_2 = 0
 
         for line in fh:
             part_1 += fuel_required(line)
+            part_2 += total_fuel_required(line)
 
     print(f"The solution to part 1 is {part_1}.")
+    print(f"The solution to part 2 is {part_2}.")
 
 
 def fuel_required(fuel):
     return int(math.floor(float(fuel) / 3 - 2))
+
+
+def total_fuel_required(fuel):
+    total_fuel = 0
+
+    while True:
+        fuel = fuel_required(fuel)
+        if fuel > 0:
+            total_fuel += fuel
+        else:
+            break
+
+    return total_fuel
 
 
 if __name__ == '__main__':
