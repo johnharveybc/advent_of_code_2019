7ccb74ddb755e0d5dde63ea579c5741f621f8b33
John Harvey <johnharveybc@gmail.com>
2020-01-29 22:07:37 -0800

    Add solution for day 2, part 2

--- solutions/02.py
+++ solutions/02.py
@@ -40,6 +40,16 @@ class IntCode:
 def main():
     input_file = '../inputs/02.txt'
 
+    sol_1 = part_1(input_file)
+
+    print(f"The solution to part 1 is {sol_1}.")
+
+    sol_2 = part_2(input_file)
+
+    print(f"The solution to part 2 is {sol_2}.")
+
+
+def part_1(input_file):
     intcode = IntCode(input_file)
 
     intcode.set_reg_val(1, 12)
@@ -47,9 +57,18 @@ def main():
 
     intcode.run_intcode()
 
-    part_1 = intcode.get_reg_val(0)
+    return intcode.get_reg_val(0)
 
-    print(f"The solution to part 1 is {part_1}.")
+
+def part_2(input_file):
+    for i in range(100):
+        for j in range(100):
+            intcode = IntCode(input_file)
+            intcode.set_reg_val(1, i)
+            intcode.set_reg_val(2, j)
+            intcode.run_intcode()
+            if intcode.get_reg_val(0) == 19690720:
+                return 100*i + j
 
 
 if __name__ == '__main__':
