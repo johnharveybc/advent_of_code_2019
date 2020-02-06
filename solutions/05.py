7f988c2df329d1f4e12b1ec5c7683aef3ba65499
John Harvey <johnharveybc@gmail.com>
2020-02-05 22:04:19 -0800

    Add solution for day 5, part 2

--- solutions/05.py
+++ solutions/05.py
@@ -5,6 +5,8 @@ def main():
     input_file = '../inputs/05.txt'
     intcode = IntCode(input_file, [1])
     intcode.run()
+    intcode = IntCode(input_file, [5])
+    intcode.run()
 
 
 if __name__ == '__main__':
