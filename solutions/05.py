f351c0b9afeaece05816a9db2049adc6765176fa
John Harvey <johnharveybc@gmail.com>
2020-02-05 21:47:13 -0800

    Add solution for day 5, part 1

---
+++ solutions/05.py
@@ -0,0 +1,11 @@
+from intcode.intcode import IntCode
+
+
+def main():
+    input_file = '../inputs/05.txt'
+    intcode = IntCode(input_file, [1])
+    intcode.run()
+
+
+if __name__ == '__main__':
+    main()
