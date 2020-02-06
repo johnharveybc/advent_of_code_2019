8eb907804370c8f762a04c20cf1c68ca3d6a7f8e
John Harvey <johnharveybc@gmail.com>
2020-02-05 21:07:19 -0800

    Add solution for day 4, part 2

--- solutions/04.py
+++ solutions/04.py
@@ -4,11 +4,15 @@ MAX = 843212
 
 def main():
     sol_1 = 0
+    sol_2 = 0
     for i in range(MIN, MAX + 1):
         if is_only_increasing(i) and has_adjacent_repeats(i):
             sol_1 += 1
+            if has_adjacent_double(i):
+                sol_2 += 1
 
-    print(f"The solution to part 1 is {sol_1}")
+    print(f"The solution to part 1 is {sol_1}.")
+    print(f"The solution to part 2 is {sol_2}.")
 
 
 def is_only_increasing(number):
@@ -19,6 +23,11 @@ def is_only_increasing(number):
 def has_adjacent_repeats(number):
     num_string = str(number)
     return any([num_string.count(c) > 1 for c in num_string])
+
+
+def has_adjacent_double(number):
+    num_string = str(number)
+    return any([num_string.count(c) == 2 for c in num_string])
 
 
 if __name__ == '__main__':
